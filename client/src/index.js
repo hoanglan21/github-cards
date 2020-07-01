46febe5e9226aef15b53e17c0727566152f90794
Lan Pham <phaml@dickinson.edu>
2020-07-01 14:18:11 +0700

    first push

--- client/src/index.js
+++ client/src/index.js
@@ -15,3 +15,48 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const gitHubForm = document.getElementById('gitHubForm');
+
+gitHubForm/addEventListener('submit', (e) => {
+
+  e.preventDefault();
+
+  let usernameInput = document.getElementById('usernameInput');
+
+  let gitHubUsername = usernameInput.value ;
+
+  requestUserRepos(gitHubUsername);
+})
+
+function requestUserRepos(username) {
+  const xhr = new XMLHttpRequest();
+
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  xhr.open('GET', url, true)
+
+  xhr.onload = function() {
+    const data = JSON.parse(this.response);
+
+    console.log(data);
+
+    for (let i in data) {
+      let ul = document.getElementById('userRepos');
+
+      let li = document.getElementById('li');
+
+      li.classList.add('list-group-item')
+
+      li.innerHTML = (`
+        <p>Repo: ${data[i].name}</p>
+        <p>Description: ${data[i].description}</p>
+        <p>URL: <a href='${data[i].html_url}'>${data[i].html_url}</a></p>
+      `);
+
+      ul.appendChild(li);
+    }
+  }
+
+  xhr.send()
+}
